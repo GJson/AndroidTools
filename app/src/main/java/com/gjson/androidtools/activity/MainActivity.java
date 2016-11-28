40dfae9c80d40e52dd26a4c54da134cce2cdd35e
GJson <gjson@gjson.com>
2016-11-28 18:38:54 +0800

    add  jni

--- app/src/main/java/com/gjson/androidtools/activity/MainActivity.java
+++ app/src/main/java/com/gjson/androidtools/activity/MainActivity.java
@@ -1,8 +1,10 @@
 package com.gjson.androidtools.activity;
 
-import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +13,7 @@ import com.gjson.androidtools.R;
 import com.gjson.androidtools.commonview.VerticalAutoScrollView;
 import com.gjson.androidtools.entity.AdInfo;
 import com.gjson.androidtools.rxjavaoretrofit.view.activities.WeatherActivity;
+import com.gjson.androidtools.utils.GjsonJni;
 import com.gjson.androidtools.utils.ToastManager;
 
 import java.util.ArrayList;
@@ -22,6 +25,7 @@ public class MainActivity extends BaseActivity {
     private Button mRxRetrofitBtn;
     private Button mBezierBtn, mCheckPermisBtn;
 
+    private Activity activity = this;
 
     // 退出时间
     private long exitTime = 0;
@@ -29,7 +33,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        ToastManager.showToast(mContext, GjsonJni.sayHello(), ToastManager.TOAST_FLAG_SUCCESS);
 
     }
 
@@ -73,17 +77,20 @@ public class MainActivity extends BaseActivity {
         });
 
         mCheckPermisBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                checkPermission(new CheckPermListener() {
-                    @Override
-                    public void superPermission() {
-                        ToastManager.showToast(mContext, "相机可用", ToastManager.TOAST_FLAG_SUCCESS);
-                    }
-                }, R.string.camera, Manifest.permission.CAMERA);
+                                               @Override
+                                               public void onClick(View v) {
 
-            }
-        });
+
+                                                   if (!isMiuiFloatWindowOpAllowed(mContext)) {
+                                                       Intent intent1 = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                                       Uri uri = Uri.fromParts("package", activity.getPackageName(), null);
+                                                       intent1.setData(uri);
+                                                       startActivity(intent1);
+                                                   }
+                                               }
+                                           }
+        );
+
     }
 
     /*获取数据*/
---
+++ app/src/main/java/com/gjson/androidtools/utils/Constant.java
@@ -0,0 +1,17 @@
+package com.gjson.androidtools.utils;
+
+/**
+ * Created by gjson on 16/11/9.
+ * Name Constant
+ * Version 1.0
+ */
+public class Constant {
+
+
+    //alibaichuan hotfix
+    public static final String ALIBAICHUAN_HOTFIX_APP_ID = "your id";
+
+    public static final String ALIBAICHUAN_HOTFIX_APP_SECRET = "you secret";
+
+    public static final String ALIBAICHUAN_HOTFIX_APP_RSA = "your rsa";
+}
